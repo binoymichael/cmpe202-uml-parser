6dc4ae642566e0b7782e5654f60e6fb72628a639
Binoy Prakash <prakash.binoy@gmail.com>
2017-02-20 21:27:12 -0800

    Changed code to a JRuby project structure

--- jruby/Hello.java
+++
@@ -1,6 +0,0 @@
-class Hello {
-    private String str;
-    public String getStr() { return this.str ; }
-    public void   setStr(String value) { this.str = value ; }
-}
-
---
+++ test/example1/A.java
@@ -0,0 +1,19 @@
+ 
+
+import java.util.Collection;
+
+public class A {
+ 
+  private int x;
+   
+  private int[] y;
+   
+  private Collection<B> b;
+	 
+	private C[] c;
+	 
+	private Collection<D> d;
+	 
+}
+ 
+//{"A"=>{:type=>:class, :modifier=>"public", :children=>{"x"=>{:modifier=>"private", :type=>"int"}, "y"=>{:modifier=>"private", :type=>"int[]"}, "b"=>{:modifier=>"private", :type=>"Collection<B>"}, "c"=>{:modifier=>"private", :type=>"C"}, "d"=>{:modifier=>"private", :type=>"Collection<D>"}}}}
