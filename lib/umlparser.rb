b5b6ebf3cb2e6398ca87f9a6d4476796d0b459da
Binoy Prakash <prakash.binoy@gmail.com>
2017-02-22 14:54:25 -0800

    Code to generate basic UML diagram

--- lib/umlparser.rb
+++ lib/umlparser.rb
@@ -45,6 +45,7 @@ class Java::ComGithubJavaparserAstBody::ConstructorDeclaration
       type: :constructor,
       modifier: modifiers.any? ? modifiers.first.name.downcase : nil,
       parameters: params,
+      visibility: true,
     }
   end
 end
@@ -71,6 +72,7 @@ class Java::ComGithubJavaparserAstBody::MethodDeclaration
       modifier: modifiers.any? ? modifiers.first.name.downcase : nil,
       return_type: data_type,
       parameters: params,
+      visibility: true,
     }
   end
 end
@@ -124,6 +126,7 @@ class Java::ComGithubJavaparserAstBody::VariableDeclarator
       data_type: data_type,
       is_association: association?,
       is_collection: collection?,
+      visibility: true,
     }
   end
 end
@@ -153,10 +156,120 @@ end
 
 #java_file_contents = File.read(ARGV[0])
 
+
+
+class UmlGraph
+  attr_accessor :ast
+  def initialize(ast)
+    @ast = ast.dup
+  end
+
+  def scrub!
+    ast.each do |klass_name, klass_props|
+      klass_props[:children].each do |child_name, child_props|
+        if child_props[:type] == :attribute && child_props[:is_association] && child_props[:visibility]
+          # check for reverse associations
+          data_type = child_props[:data_type] 
+          if ast.key?(data_type)
+            reverse_associations = ast[data_type][:children].select do |k, v|
+                                      v[:data_type] == klass_name && 
+                                      v[:type] == :attribute &&
+                                      v[:is_association]
+                                   end
+            reverse_associations.each do |k, v|
+              ast[data_type][:children][k][:visibility] = false
+            end
+          else
+            # Make association an inline attribute if not present at AST root level
+            child_props[:is_association] = false
+          end
+        end
+      end
+    end
+  end
+
+  def write(filename)
+    scrub!
+    File.open(filename, 'w') do |f|
+      result = []
+      ast.each do |klass, props|
+        uml_graph(klass, props).each do |line|
+          f.puts line
+        end
+      end
+    end
+    #puts result
+  end
+
+  def uml_graph(klass, props)
+    header_string = "%{modifier} %{type} %{name}" % {modifier: props[:modifier], type: props[:type], name: klass}
+    attributes = []
+    associations = []
+
+    props[:children].each do |child, c_props|
+      next if c_props[:visibility] == false
+
+      formatted_props = c_props.merge(name: child)
+      formatted_props[:data_type] += '[]' if formatted_props[:is_collection]
+
+      if c_props[:is_association]
+        association_arity = c_props[:is_collection] ? '0..*' : '0..1'
+        associations << [association_arity, c_props[:data_type]]
+      else
+        attributes << "%{modifier} %{data_type} %{name};" % formatted_props
+      end
+    end
+    uml_graph_templatify(header_string, attributes, associations)
+  end
+
+  def uml_graph_templatify(header_string, attributes, associations)
+    lines = []
+    if associations.any?
+      lines << '/**'
+      associations.each do |assoc|
+        lines << " * @assoc - - #{assoc.first} #{assoc.last}"
+      end
+      lines << ' */'
+    end
+
+    lines << header_string
+    lines << '{'
+      attributes.each do |attribute|
+        lines << "\t#{attribute}"
+      end
+    lines << '}'
+    lines << "\n"
+  end
+
+end
+
 @tree = {}
-Dir.glob("#{ARGV[0]}/*.java").each do |filename|
+source = ARGV[0]
+Dir.glob("#{source}/*.java").each do |filename|
   cu = JavaParser.parse(File.read(filename))
   AstVisitor.new.visit(cu, @tree)
 end
 
-pp @tree
+output_file = "#{source}.java"
+UmlGraph.new(@tree).write(output_file)
+dot_file = "#{source}.dot"
+output_png = "#{source}.png"
+
+
+require 'open3'
+commands = ["java", "-jar", "lib/umlgraph-5.7.2.23.jar", "-private", "-attributes", "-types", "-visibility", "-output", dot_file]
+commands << output_file
+output, status = Open3.capture2(*commands)
+puts output            # -> "hello; rm -rf *\n"
+puts status.pid        # 123 or the process id
+puts status.exitstatus # 0
+
+
+commands = ["dot", "-T", "png", "-o", output_png, dot_file]
+p commands.join(' ')
+output, status = Open3.capture2(*commands)
+puts output            # -> "hello; rm -rf *\n"
+puts status.pid        # 123 or the process id
+puts status.exitstatus # 0
+
+
