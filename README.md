98319bbc8022b28a3c2307d28e23e9a4c26e38d1
Binoy Prakash <prakash.binoy@gmail.com>
2017-04-27 12:57:50 -0700

    Update readme with test instructions

--- README.md
+++ README.md
@@ -2,27 +2,31 @@
 # cmpe202-uml-parser
 CMPE202 Project
 
-# Create AST Tree
- - ruby lib/parser.rb test
+# Install pre-requisites
+sudo apt-get install default-jre
+sudo apt-get install graphviz
+sudo apt-get install aspectj
+sudo apt-get install plotutils
 
-# Make UML class diagram
-  - java -jar lib/umlgraph-5.7.2.23.jar -private -attributes -visibility -output - uml.java | dot -Tpng -ograph.png
+# Download test file for class diagram
+sudo apt-get install unzip
+curl -LO https://www.dropbox.com/s/tt2lo4il5qwzwmn/test1.zip
+mkdir input
+unzip test1.zip -d input
 
+# Download parser script
+curl -LO https://www.dropbox.com/s/rj8b73br7x5kwgq/parser.tar.gz
+mkdir parser
+tar -xvzf parser.tar.gz -C parser
 
-# Prerequisties
- - Java version 1.8 or above
- ```
- sudo apt-get install default-jre
- ```
+# Download test file for sequence diagram
+curl -LO https://www.dropbox.com/s/h8lj7igox1ao8mn/sequence.zip
+mkdir sequence
+unzip sequence.zip -d sequence
 
- - Graphviz
- ```
- sudo apt-get install graphviz
- ```
+# Draw class diagram
+cd parser
+./umlparser.sh ../input test1
 
- - AspectJ
- ```
- sudo apt-get install aspectj
- ```
-
-
+# Draw sequence diagram
+./umlparser.sh -s ../sequence sequencetest
---
+++ bin/umlparser.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+# Check for java
+if [ -z `which java` ]; then
+  echo "This program needs Java version 1.8 or above to work correctly. Please install it and retry.";
+  exit 1;
+fi
+
+sequenceflag=false
+usage () { echo "Usage: ./umlparser.sh [options] input-folder output-file";
+           echo "";
+           echo "Prints class diagram by default";
+           echo "Use -s flag to draw sequence diagram instead"; }
+
+while getopts ":sh" opt; do
+  case ${opt} in
+    h ) usage
+      exit 0
+      ;;
+    s ) sequenceflag=true
+      break;
+      ;;
+    \? ) usage
+      exit 1
+      ;;
+  esac
+done
+shift $(($OPTIND - 1))
+
+if [[ $# -lt 2 ]] ; then
+    usage
+    exit 1
+fi
+
+if ! $sequenceflag
+then
+  # Check for graphviz
+  if [ -z `which dot` ]; then
+    echo "This program needs the graphviz package installed to work correctly. Please install it and retry."
+    exit 1
+  fi
+
+  #Cleanup exisiting .java & .png files
+  [ -e "$2.java" ] && rm "$2.java"
+  [ -e "$2.png" ] && rm "$2.png"
+
+  echo 'Preparing class diagram ...'
+  java -jar uml-class-diagram.jar $1 $2
+  echo "Drawing $2.png"
+  java -jar umlgraph-5.7.2.23.jar -private -attributes -operations -types -visibility -output - "$2.java" | dot -Tpng -Gdpi=200 -o"$2.png" 
+else
+  # Check for ajc
+  if [ -z `which ajc` ]; then
+    echo "This program needs the aspectj compiler installed to work correctly. Please install it and retry."
+    exit 1
+  fi
+
+  if [ -z `which pic2plot` ]; then
+    echo "This program needs the plotutils package installed to work correctly. Please install it and retry."
+    exit 1
+  fi
+
+  echo 'Preparing sequence diagram ...'
+  tmpdir=`mktemp -d`
+  cp "$1/"*".java" $tmpdir
+  cp TracingAspect.aj $tmpdir
+  ajc -1.8 $tmpdir/*.java $tmpdir/*.aj
+  java -cp ".:./aspectjrt-1.8.8.jar:$tmpdir" Main
+  pic2plot -Tsvg  uml.pic > "$2.svg.tmp"
+  awk 'NR==1,/transform/{sub(/transform=".*" xml/, "transform=\"translate(0.25,0.1) scale(0.05, -0.05)\" xml")} 1' "$2.svg.tmp" > "$2.svg"
+
+  # Cleaning up files
+  rm -rf $tmpdir
+  rm $2.svg.tmp
+fi
